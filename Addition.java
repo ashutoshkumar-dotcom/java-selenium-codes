ce04106e015370e986a72ba64e20adb5ed614aae
Ashutosh <ashutosh.kumar84y@gmail.com>
2025-04-03 02:41:52 +0530

    first program

---
+++ Addition.java
@@ -0,0 +1,15 @@
+package com.list.programs;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10, b=20, c;
+		c = a + b;
+		System.out.println("Addition of two numbers is: " +c);
+		
+
+	}
+
+}
